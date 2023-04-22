795e3fdcfa0c64070171292b76a85665eadaa20e
makodmytro <makodmytro@gmail.com>
2023-04-22 11:43:37 +0900

    create ringcentral pakage

---
+++ @ringcentral/sdk/es6/core/Constants.js
@@ -0,0 +1,6 @@
+// var version = require('../../package.json').version; //eslint-disable-line
+import {version} from '../../package.json';
+// do not use import here
+export { version };
+export var authResponseProperty = 'RCAuthorizationResponse';
+//# sourceMappingURL=Constants.js.map
